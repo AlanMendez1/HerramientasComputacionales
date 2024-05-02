36820f67bc065ecf01038ff5a70b2473ff6ba285
Alan Méndez <alanmendez200403@gmail.com>
2024-05-01 20:43:20 -0500

    Agregar suma a su rama

--- mult.py
+++
@@ -1,5 +0,0 @@
-valor1 = int(input("Escriba un primer número: "))
-valor2 = int(input("Escriba un segundo número: "))
-resultado = valor1 * valor2
-
-mult = print(f'{valor1} * {valor2} = {resultado}')
